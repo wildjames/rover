6681255dd58d4f188422e1fe5f7cbf39d8fefecd
James Wild <wild.james343@gmail.com>
2023-01-15 13:36:41 +0000

    Slightly better reporting in the install

--- setup/install_service.sh
+++ setup/install_service.sh
@@ -4,14 +4,17 @@
 # We will create them in the /var/log directory
 echo "Creating log file directory"
 mkdir -p /home/rover/log
-chmod -cR 666 /home/rover/log
+chmod -cR 777 /home/rover/log
 chgrp -cR adm /home/rover/log
 
 
 # TODO: Apache setup should go here. Dockerise that?
 echo "Doing Apache2 service check"
 systemctl restart apache2.service
-sleep 5
+echo "Is the Apache2 service running?"
+systemctl is-active apache2.service
+echo "Is the Apache2 service enabled?"
+systemctl is-enabled apache2.service
 
 # This script installs the rover controller service
 # It should be run as root
@@ -20,8 +23,8 @@ cp rover_controller.service /etc/systemd/system/
 systemctl enable rover_controller.service
 echo "Starting rover controller service"
 systemctl start rover_controller.service
-sleep 5
-systemctl status rover_controller.service
+echo "Is the rover controller service running?"
+systemctl is-active rover_controller.service
 
 
 echo "OK!"
