95d01e9e466ff44d7ebf6a7a601c60ca87205fdd
James Wild <wild.james343@gmail.com>
2023-01-15 11:55:04 +0000

    Fix service file

--- setup/install_controller_service.sh
+++ setup/install_controller_service.sh
@@ -2,6 +2,8 @@
 
 # This script installs the rover controller service
 # It should be run as root
-cp controller.service /etc/systemd/system/
-systemctl enable controller.service
-systemctl start controller.service
+cp rover_controller.service /etc/systemd/system/
+systemctl enable rover_controller.service
+systemctl start rover_controller.service
+sleep 5
+systemctl status rover_controller.service
