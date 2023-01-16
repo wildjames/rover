ee4e2949be01456b8a5f86904ad6d0dfd1cf29ed
James Wild <wild.james343@gmail.com>
2023-01-16 21:03:24 +0000

    hopeful fix

--- controllers/controller_server.py
+++ controllers/controller_server.py
@@ -1,9 +1,10 @@
 import json
 import logging
 
-import led_control
 import camera_control
-from bottle import get, post, request, response, run
+import cv2
+import led_control
+from bottle import Response, get, post, request, response, run
 
 logging.basicConfig(
     filename="/home/rover/log/rover_controller.log",
@@ -14,12 +15,34 @@ logging.basicConfig(
 )
 
 
+camera = cv2.VideoCapture(0)
+logging.info("Camera initialized? {}".format(camera.isOpened()))
+
+
+def gen_frames():
+    """Video streaming generator function."""
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            logging.error("Rover failed to read camera frame")
+            break
+        else:
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+            )  # concat
+
+
 @get("/video_feed")
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     # Hook into openCV to get the camera feed
     logging.info("Rover received video feed request")
-    return response(camera_control.gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(
+        camera_control.gen_frames(),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
 
 
 @post("/led_command")
