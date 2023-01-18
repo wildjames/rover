2d09a82a2f611e80b7ff6a9fedb1692e7b675430
James Wild <wild.james343@gmail.com>
2023-01-18 00:23:03 +0000

    fix js

--- django_frontend/static/main.js
+++ django_frontend/static/main.js
@@ -5,9 +5,9 @@
         var isStreaming = false;
         var start = document.getElementById('start');
         var stop = document.getElementById('stop');
+        var led0 = document.getElementById('led0');
         var led1 = document.getElementById('led1');
         var led2 = document.getElementById('led2');
-        var led3 = document.getElementById('led3');
         var canvas = document.getElementById('c');
         var video = document.getElementById('v');
         var ctx = canvas.getContext('2d');
@@ -16,6 +16,29 @@
         setInterval(function () {
             console.log("I will update the state of the LEDs");
         }, 5000);
+
+        led0.addEventListener('click', function (e) {
+            // I need to send a toggle message to the server. First, get the state of the LED
+            var state = led0.getAttribute('data-state');
+            state = parseInt(state);
+
+            // the LED index is the last character of the id
+            var ledIndex = led0.id[led0.id.length - 1];
+            ledIndex = parseInt(ledIndex);
+
+            var newState = state === 1 ? 0 : 1;
+            console.log("LED " + ledIndex + ". Old state: " + state + ", new state: " + newState);
+            
+            // Send a command POST request with a JSON payload to the controller server
+            var xhr = new XMLHttpRequest();
+            // Make the post request asynchronously
+            xhr.open('POST', '/rover/led_control', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            // The json payload is of the form {"states": [[1, newstate]]}
+            var payload = JSON.stringify({"states": [[ledIndex, newState]]});
+            console.log("Sending payload: " + payload);
+            xhr.send(payload);
+        }, false);
 
         led1.addEventListener('click', function (e) {
             // I need to send a toggle message to the server. First, get the state of the LED
@@ -43,16 +66,24 @@
         led2.addEventListener('click', function (e) {
             // I need to send a toggle message to the server. First, get the state of the LED
             var state = led2.getAttribute('data-state');
-            var newState = state === '1' ? '0' : '1';
-            console.log("LED 2. Old state: " + state + ", new state: " + newState);
-        }, false);
+            state = parseInt(state);
 
-        led3.addEventListener('click', function (e) {
-            // I need to send a toggle message to the server. First, get the state of the LED
-            var state = led3.getAttribute('data-state');
-            var newState = state === '1' ? '0' : '1';
-            console.log("LED 3. Old state: " + state + ", new state: " + newState);
-        }, false);
+            // the LED index is the last character of the id
+            var ledIndex = led2.id[led2.id.length - 1];
+            ledIndex = parseInt(ledIndex);
+
+            var newState = state === 1 ? 0 : 1;
+            console.log("LED " + ledIndex + ". Old state: " + state + ", new state: " + newState);
+            
+            // Send a command POST request with a JSON payload to the controller server
+            var xhr = new XMLHttpRequest();
+            // Make the post request asynchronously
+            xhr.open('POST', '/rover/led_control', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            // The json payload is of the form {"states": [[1, newstate]]}
+            var payload = JSON.stringify({"states": [[ledIndex, newState]]});
+            console.log("Sending payload: " + payload);
+            xhr.send(payload);        }, false);
 
         start.addEventListener('click', function (e) {
             var address = '192.168.1.170:1002/webrtc';
