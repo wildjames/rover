c5578013f4824878488788b524d609f120878c2f
James Wild <wild.james343@gmail.com>
2023-01-21 22:14:30 +0000

    tweaks to the ESC

--- controllers/motor_control.py
+++ controllers/motor_control.py
@@ -5,31 +5,35 @@ from typing import List
 # Interface for controlling an ESC (Electronic Speed Controller) using a Raspberry Pi
 # and a PWM (Pulse Width Modulation) signal.
 
-ESC_control_pins = [18]
+ESC_control_pins = [12]
 
 
 class ESCController:
     """Class for controlling an ESC using a Raspberry Pi and a PWM signal."""
 
-    started = False
+    armed = False
 
     def __init__(
         self,
         pin,
-        frequency: int = 100,
-        min_pulse_width: int = 0.0,
-        max_pulse_width: int = 1.0,
+        frequency: int = 50,
+        min_pulse_width: int = 1000,
+        max_pulse_width: int = 2000,
     ):
-        """The constructor for the ESC class."""
+        """The constructor for the ESC class.
+        
+        Frequency is the frequency of the PWM signal in Hz.
+        min/max pulse width is the minimum and maximum pulse widths, in microseconds
+        """
 
         # Create a PWMOutputDevice object to control the ESC.
         self.pwm = gpiozero.PWMOutputDevice(pin, frequency=frequency, initial_value=0)
 
-        # Set the minimum and maximum pulse widths.
-        self.min_pulse_width = min_pulse_width
-        self.max_pulse_width = max_pulse_width
+        # calculate the duty cycle that corresponds to the minumum and maximum pulse widths, from the frequency
+        self.min_pulse_width = frequency * min_pulse_width / 1000000
+        self.max_pulse_width = frequency * max_pulse_width / 1000000
 
-    def start(self):
+    def arm(self):
         """Start the ESC wakeup handshake.
 
         Each step is separated by a 2 second delay:
@@ -56,18 +60,18 @@ class ESCController:
 
         # Then we're done
         self.pwm.value = 0.0
-        self.started = True
+        self.armed = True
 
     def stop(self):
         """Stop the ESC."""
 
         # Set the pulse width to 0.
-        self.pwm.value = 0
+        self.pwm.off()
 
     def set_speed(self, speed: float):
         """Set the speed of the ESC, as a percentage of the maximum speed."""
-        if not self.started:
-            self.start()
+        if not self.armed:
+            self.arm()
 
         # Set the pulse width to the given speed.
         pwm_value = (self.max_pulse_width - self.min_pulse_width) * (speed / 100.0)
