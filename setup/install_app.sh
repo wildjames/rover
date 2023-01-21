061d6a43787553c0cc434a85ac3800dea5d9ad42
James Wild <wild.james343@gmail.com>
2023-01-21 17:30:42 +0000

    have service use sudo, rather than run as root

--- setup/install_app.sh
+++ setup/install_app.sh
@@ -25,31 +25,15 @@ apt-get install libwayland-cursor0 libxfixes3 libva2 \
 
 python3 -m pip install -r ../requirements.txt
 
-# There are some logging files that we will want to create.
-# We will create them in the /var/log directory
-echo "Creating/cleaning log file directory"
-rm -r /home/rover/log
-mkdir -p /home/rover/log
-chmod -cR 777 /home/rover/log
-chgrp -cR adm /home/rover/log
-
-# TODO: Apache setup should go here. Dockerise that?
-echo "Doing Apache2 service check"
-systemctl restart apache2.service
-echo "Is the Apache2 service running?"
-systemctl is-active apache2.service
-echo "Is the Apache2 service enabled?"
-systemctl is-enabled apache2.service
-
 # This script installs the rover controller service
 # It should be run as root
 echo "Installing rover controller service"
-cp rover_controller.service /etc/systemd/system/
-systemctl enable rover_controller.service
+cp hardware_controller.service /etc/systemd/system/
+systemctl enable hardware_controller.service
 echo "Starting rover controller service"
-systemctl start rover_controller.service
+systemctl start hardware_controller.service
 echo "Is the rover controller service running?"
-systemctl is-active rover_controller.service
+systemctl is-active hardware_controller.service
 
 # Install the websites, too
 
