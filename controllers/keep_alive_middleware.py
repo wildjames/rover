5e19fa193f3f829e65a99825327c5ca96340663a
James Wild <wild.james343@gmail.com>
2023-01-29 19:20:53 +0000

    Fix logging

--- controllers/keep_alive_middleware.py
+++ controllers/keep_alive_middleware.py
@@ -1,4 +1,5 @@
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -18,10 +19,11 @@ last_message = time.time()
 
 def keep_alive(f):
     """When the call is triggered, update the last activity to the current time."""
+
     @wraps(f)
     def decorated(*args, **kwargs):
         global last_message
-        
+
         delta_time = time.time() - last_message
         logger.debug("Recieved a message that registers as keepalive activity.")
         last_message = time.time()
@@ -32,28 +34,39 @@ def keep_alive(f):
 
 
 def check_inactivity():
-    """Check how long its been since the last activity. 
+    """Check how long its been since the last activity.
     If it's been more than SLEEP_THRESHOLD seconds, send the computer to sleep."""
     global last_message
 
     delta_time = time.time() - last_message
-    
-    # Add 10 seconds to the earliest time I would sleep.
-    time_to_next_check = (SLEEP_THRESHOLD - delta_time) + 10 
 
-    logger.info("Checking last activity... It has been {:.3f} seconds since a command".format(delta_time))
+    logger.info(
+        "Checking last activity... It has been {:.3f} seconds since a command".format(
+            delta_time
+        )
+    )
     if delta_time > SLEEP_THRESHOLD:
         if ENABLE_SLEEP:
             logger.critical("Inactivity threshold reached. Sending computer to sleep.")
             # Easy way
             from subprocess import call
+
             call("sudo shutdown -P now", shell=True)
         else:
-            logger.critical("Would have sent the computer to sleep")
-            last_message = time.time()
+            last_message = time.time() - 10
+            delta_time = time.time() - last_message
+            logger.critical(
+                "Would have sent the computer to sleep. Set last message time to {:.3f} seconds ago.".format(
+                    delta_time
+                )
+            )
 
         # TODO: Send a signal to the sleepypi to shutdown the raspberry pi.
 
-    logger.info("Will check for inactivity again in {:.3f} seconds".format(time_to_next_check))
-    
+    # Add 10 seconds to the earliest time I would sleep.
+    time_to_next_check = (SLEEP_THRESHOLD - delta_time) + 10
+    logger.info(
+        "Will check for inactivity again in {:.3f} seconds".format(time_to_next_check)
+    )
+
     threading.Timer(time_to_next_check, check_inactivity).start()
