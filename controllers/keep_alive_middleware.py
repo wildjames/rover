382f24e8f17c44e965c6eaf89269dcc7e6cddc2b
James Wild <wild.james343@gmail.com>
2023-01-29 17:45:23 +0000

    enable the sleep mode

--- controllers/keep_alive_middleware.py
+++ controllers/keep_alive_middleware.py
@@ -10,6 +10,8 @@ import threading
 
 # Time in seconds to go to sleep after inactivity.
 SLEEP_THRESHOLD = 300
+# Enable variable
+ENABLE_SLEEP = True
 
 last_message = time.time()
 
@@ -32,16 +34,21 @@ def keep_alive(f):
 def check_inactivity():
     """Check how long its been since the last activity. 
     If it's been more than SLEEP_THRESHOLD seconds, send the computer to sleep."""
+    global last_message
+
     delta_time = time.time() - last_message
     time_to_next_check = SLEEP_THRESHOLD - delta_time
 
     logger.info("Checking last activity... It has been {:.3f} seconds since a command".format(delta_time))
     if delta_time > SLEEP_THRESHOLD:
-        logger.warning("I NEED TO SLEEP")
-        # Easy way
-        # from subprocess import call
-        # call("sudo shutdown -P now", shell=True)
-        
+        if ENABLE_SLEEP:
+            # Easy way
+            from subprocess import call
+            call("sudo shutdown -P now", shell=True)
+        else:
+            logger.info("Would have sent the computer to sleep")
+            last_message = time.time()
+            
         # TODO: Send a signal to the sleepypi to shutdown the raspberry pi.
 
     logger.info("Will check for inactivity again in {:.3f} seconds".format(time_to_next_check))
